12df64e16f0290a70a20cebd7c6376dbceb9dfbb
Antoni Goldstein <antek.goldstein@gmail.com>
2026-07-31 14:37:04 +0200

    chore: refactor data operations modals into a new file
    + some cleanup

--- web/src/react-app-env.d.ts
+++ web/src/react-app-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="react-scripts" />
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module 'bootstrap-icons/font/bootstrap-icons.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
