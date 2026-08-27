a60ecfadeba8a35dc2b4bdae16c7deed526ad077
Antoni Goldstein <antek.goldstein@gmail.com>
2026-08-27 17:41:17 +0200

    chore(web): es6 and necessary config changes

--- web/src/components/Tooltip.tsx
+++ web/src/components/Tooltip.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, ReactNode } from 'react'
 import { OverlayTrigger, Tooltip } from 'react-bootstrap'
-import { Placement } from 'react-bootstrap/esm/types'
+import { Placement } from 'react-bootstrap/types'
 
 export const TTip: React.FC<{ placement?: Placement; text: ReactNode; children: ReactElement }> = ({
   placement,
